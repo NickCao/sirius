ef85ab8313b3de81c77a879ed4c6e60e828904c9
Nick Cao <nickcao@nichi.co>
2022-06-28 14:53:54 +0800

    cleanup

--- flake.nix
+++ flake.nix
@@ -2,40 +2,31 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
-        let pkgs = import nixpkgs { inherit system; overlays = [ self.overlay rust-overlay.overlay ]; }; in
+        let pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; }; in
         rec {
           defaultPackage = pkgs.sirius;
           devShell = pkgs.mkShell {
-            buildInputs = [ (pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override { extensions = [ "rust-analyzer-preview" "rust-src" ]; })) ];
+            nativeBuildInputs = [ pkgs.rust-analyzer ];
+            inputsFrom = [ defaultPackage ];
           };
         }
       ) //
     {
-      overlay = final: prev:
-        let
-          toolchain = final.rust-bin.nightly.latest.default;
-          platform = final.makeRustPlatform { cargo = toolchain; rustc = toolchain; };
-        in
-        {
-          sirius = platform.buildRustPackage {
-            name = "sirius";
-            src = final.lib.cleanSource ./.;
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-              outputHashes = {
-                "libnar-0.1.0" = "sha256-rzAxomiOtMuvJMTvbMnW12POUV5t177wgdi3pAHSjFE=";
-              };
+      overlay = final: prev: {
+        sirius = final.rustPlatform.buildRustPackage {
+          name = "sirius";
+          src = self;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "libnar-0.1.0" = "sha256-rzAxomiOtMuvJMTvbMnW12POUV5t177wgdi3pAHSjFE=";
             };
           };
         };
+      };
     };
 }
